6d5f3fd02c002a5b9dc2215521d8494cdee39e79
mikolaj398 <mikolaj.baran16@gmail.com>
2022-04-04 19:39:34 +0200

    Add plots

--- main.py
+++ main.py
@@ -1,20 +1,28 @@
 import warnings
-
 warnings.filterwarnings("ignore")
 
+import sys
 import numpy as np
-import matplotlib.pyplot as plt
 from itertools import chain
 from keras.preprocessing.image import ImageDataGenerator
 from sklearn.model_selection import train_test_split
 
 from data_prep import get_combined_datasets
 from consts import *
+from plots import *
+
+
+plot = False
+if len(sys.argv) != 1:
+    plot = bool(sys.argv[1])
 
 data = get_combined_datasets()
 
 all_labels = np.unique(data["Finding Labels"].map(lambda x: x.split("|")).tolist())
 all_labels = [x for x in set(chain(*all_labels)) if len(x) > 0]
+
+if plot:
+    plot_class_count(data, all_labels)
 
 train_df, test_df = train_test_split(data, test_size=0.25, random_state=2018)
 train_df["Finding Labels"] = train_df.apply(lambda x: x["Finding Labels"].split("|"), axis=1)
@@ -43,15 +51,5 @@ train_gen = img_data_gen.flow_from_dataframe(
     batch_size=8,
 )
 
-all_labels = list(train_gen.class_indices.keys())
-
-iter_image, iter_labels = next(train_gen)
-fig, ax = plt.subplots(2, 4)
-for image, labels, plot in zip(iter_image, iter_labels, ax.flatten()):
-    plot.imshow(image[:, :, 0], cmap="bone")
-    plot.set_title(
-        ", ".join([class_name for class_name, belongs in zip(all_labels, labels) if belongs == 1])
-    )
-    plot.axis("off")
-
-plt.show()
+if plot:
+    show_image_examples(train_gen)
