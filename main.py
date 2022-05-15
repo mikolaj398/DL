906d33a3b6a15192a9903fae2bffb60c0e571b9c
mikolaj398 <mikolaj.baran16@gmail.com>
2022-05-15 15:19:00 +0200

    save work

--- main.py
+++ main.py
@@ -48,8 +48,88 @@ train_gen = img_data_gen.flow_from_dataframe(
     class_mode="categorical",
     target_size=IMG_SIZE,
     color_mode="grayscale",
-    batch_size=8,
+    batch_size=32,
 )
 
 if plot:
     show_image_examples(train_gen)
+
+test_gen = img_data_gen.flow_from_dataframe(
+    dataframe=test_df,
+    x_col="path",
+    y_col="Finding Labels",
+    class_mode="categorical",
+    target_size=IMG_SIZE,
+    color_mode="grayscale",
+    batch_size=1024,
+)
+
+from keras.applications.mobilenet import MobileNet
+from keras.layers import GlobalAveragePooling2D, Dense, Dropout, Flatten
+from keras.models import Sequential
+
+t_x, t_y = next(train_gen)
+test_X, test_Y = next(test_gen)
+
+mobilenet_model = MobileNet(input_shape =  t_x.shape[1:], 
+                                 include_top = False, weights = None)
+
+multi_disease_model = Sequential()
+multi_disease_model.add(mobilenet_model)
+multi_disease_model.add(GlobalAveragePooling2D())
+multi_disease_model.add(Dropout(0.5))
+multi_disease_model.add(Dense(512))
+multi_disease_model.add(Dropout(0.5))
+multi_disease_model.add(Dense(len(all_labels), activation = 'sigmoid'))
+multi_disease_model.compile(optimizer = 'adam', loss = 'binary_crossentropy',
+                           metrics = ['binary_accuracy', 'mae'])
+multi_disease_model.summary()
+
+from keras.callbacks import ModelCheckpoint, LearningRateScheduler, EarlyStopping, ReduceLROnPlateau
+weight_path="{}_weights.best.hdf5".format('xray_class')
+
+checkpoint = ModelCheckpoint(weight_path, monitor='val_loss', verbose=1, 
+                             save_best_only=True, mode='min', save_weights_only = True)
+
+early = EarlyStopping(monitor="val_loss", 
+                      mode="min", 
+                      patience=3)
+callbacks_list = [checkpoint, early]
+
+multi_disease_model.fit_generator(train_gen, 
+                                  steps_per_epoch=100,
+                                  validation_data = (test_X, test_Y), 
+                                  epochs = 10, 
+                                  callbacks = callbacks_list)
+
+for c_label, s_count in zip(all_labels, 100*np.mean(test_Y,0)):
+    print('%s: %2.2f%%' % (c_label, s_count))
+
+pred_Y = multi_disease_model.predict(test_X, batch_size = 32, verbose = True)
+
+from sklearn.metrics import roc_curve, auc
+fig, c_ax = plt.subplots(1,1, figsize = (9, 9))
+for (idx, c_label) in enumerate(all_labels):
+    fpr, tpr, thresholds = roc_curve(test_Y[:,idx].astype(int), pred_Y[:,idx])
+    c_ax.plot(fpr, tpr, label = '%s (AUC:%0.2f)'  % (c_label, auc(fpr, tpr)))
+c_ax.legend()
+c_ax.set_xlabel('False Positive Rate')
+c_ax.set_ylabel('True Positive Rate')
+fig.savefig('barely_trained_net.png')
+
+multi_disease_model.fit_generator(train_gen, 
+                                  steps_per_epoch = 100,
+                                  validation_data =  (test_X, test_Y), 
+                                  epochs = 5, 
+                                  callbacks = callbacks_list)
+
+from sklearn.metrics import roc_curve, auc
+fig, c_ax = plt.subplots(1,1, figsize = (9, 9))
+for (idx, c_label) in enumerate(all_labels):
+    fpr, tpr, thresholds = roc_curve(test_Y[:,idx].astype(int), pred_Y[:,idx])
+    c_ax.plot(fpr, tpr, label = '%s (AUC:%0.2f)'  % (c_label, auc(fpr, tpr)))
+c_ax.legend()
+c_ax.set_xlabel('False Positive Rate')
+c_ax.set_ylabel('True Positive Rate')
+fig.savefig('trained_net.png')                                 
+
